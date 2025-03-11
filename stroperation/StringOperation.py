7c8f41a282dcd63fd9e2b9c356f0cf7e9be8bfc3
poacher286 <bulandsahu50@gmail.com>
2025-03-11 16:46:02 +0530

    updated String operation

---
+++ stroperation/StringOperation.py
@@ -0,0 +1,13 @@
+name = input("Name : ")
+
+# result = len(name)
+# result = name.find("a")#find first occurance
+# result = name.rfind("a")#find last occurance
+# result = name.capitalize()
+
+
+# print("Length : "+ result)  # TypeError: can only concatenate str (not "int") to str
+# print(f"Length : {result}")
+# print(f"Capitalize : {result}")
+
+print(help(str))
